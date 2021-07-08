9282b0827ed641936b00bc0f6fceece2c3ab4a82
Rodion Kolovanov <rodion.kolovanov@inbox.ru>
2021-07-08 21:14:21 +0300

    Code refactoring.

--- src/main/java/leti/practice/algorithm/AlgorithmExecutorTest.java
+++ src/main/java/leti/practice/algorithm/AlgorithmExecutorTest.java
@@ -8,56 +8,6 @@ import org.junit.jupiter.api.Assertions;
 
 public class AlgorithmExecutorTest {
     @Test
-    public void nextStep() {
-        AlgorithmExecutor test_alg = new AlgorithmExecutor();
-        ResidualNetwork<Double> test_network = new ResidualNetwork<>();
-
-        test_network.setSource(new Node("a"));
-        test_network.setDestination(new Node("d"));
-        test_network.addEdge(new Node("a"),new Node("b"),new EdgeProperties<>(2.0,0.0));
-        test_network.addEdge(new Node("b"),new Node("c"),new EdgeProperties<>(13.0,0.0));
-        test_network.addEdge(new Node("c"),new Node("d"),new EdgeProperties<>(9.0,0.0));
-        test_network.addEdge(new Node("h"),new Node("c"),new EdgeProperties<>(4.0,0.0));
-        test_network.addEdge(new Node("e"),new Node("f"),new EdgeProperties<>(7.0,0.0));
-        test_network.addEdge(new Node("g"),new Node("h"),new EdgeProperties<>(7.0,0.0));
-        test_network.addEdge(new Node("b"),new Node("e"),new EdgeProperties<>(7.0,0.0));
-        test_network.addEdge(new Node("a"),new Node("g"),new EdgeProperties<>(8.0,0.0));
-        test_network.addEdge(new Node("f"),new Node("d"),new EdgeProperties<>(10.0,0.0));
-
-        test_alg.setNetwork(test_network);
-        test_alg.runAlgorithm(test_network);
-        ResidualNetwork<Double> test_network_after = test_network.copy();
-        test_alg.previousStep();
-        test_alg.nextStep();
-        ResidualNetwork<Double> test_network_before = test_network.copy();
-        Assertions.assertTrue(test_network_before.equals(test_network_after));
-    }
-
-    @Test
-    public void previousStep() {
-        AlgorithmExecutor test_alg = new AlgorithmExecutor();
-        ResidualNetwork<Double> test_network=new ResidualNetwork<>();
-        test_network.setSource(new Node("a"));
-        test_network.setDestination(new Node("h"));
-        test_network.addEdge(new Node("a"),new Node("c"),new EdgeProperties<>(12.0,0.0));
-        test_network.addEdge(new Node("a"),new Node("d"),new EdgeProperties<>(22.0,0.0));
-        test_network.addEdge(new Node("c"),new Node("d"),new EdgeProperties<>(14.0,0.0));
-        test_network.addEdge(new Node("c"),new Node("f"),new EdgeProperties<>(21.0,0.0));
-        test_network.addEdge(new Node("d"),new Node("g"),new EdgeProperties<>(10.0,0.0));
-        test_network.addEdge(new Node("g"),new Node("f"),new EdgeProperties<>(10.0,0.0));
-        test_network.addEdge(new Node("g"),new Node("h"),new EdgeProperties<>(7.0,0.0));
-        test_network.addEdge(new Node("f"),new Node("h"),new EdgeProperties<>(7.0,0.0));
-
-        test_alg.setNetwork(test_network);
-        test_alg.runAlgorithm(test_network);
-        ResidualNetwork<Double> test_network_after = test_network.copy();
-        test_alg.previousStep();
-        test_alg.nextStep();
-        ResidualNetwork<Double> test_network_before = test_network.copy();
-        Assertions.assertTrue(test_network_before.equals(test_network_after));
-    }
-
-    @Test
     public void runAlgorithm() {
         AlgorithmExecutor test_alg = new AlgorithmExecutor();
 
@@ -128,7 +78,8 @@ public class AlgorithmExecutorTest {
         test_network_6.addEdge(new Node("d"),new Node("e"),new EdgeProperties<>(0.0,0.0));
         test_network_6.addEdge(new Node("d"),new Node("f"),new EdgeProperties<>(0.0,0.0));
         test_network_6.addEdge(new Node("e"),new Node("c"),new EdgeProperties<>(0.0,0.0));
-        //7
+
+        // 7
         ResidualNetwork<Double> test_network_7 = new ResidualNetwork<>();
         test_network_7.setSource(new Node("a"));
         test_network_7.setDestination(new Node("f"));
@@ -141,6 +92,7 @@ public class AlgorithmExecutorTest {
         test_network_7.addEdge(new Node("d"), new Node("e"), new EdgeProperties<>(9.0,0.0));
         test_network_7.addEdge(new Node("d"), new Node("f"), new EdgeProperties<>(6.0,0.0));
         test_network_7.addEdge(new Node("e"), new Node("c"), new EdgeProperties<>(6.0,0.0));
+
         // 8
         ResidualNetwork<Double> test_network_8=new ResidualNetwork<>();
         test_network_8.setSource(new Node("a"));
@@ -200,14 +152,14 @@ public class AlgorithmExecutorTest {
         test_network_11.addEdge(new Node("d"),new Node("e"),new EdgeProperties<>(10.0,0.0));
         test_network_11.addEdge(new Node("e"),new Node("d"),new EdgeProperties<>(10.0,0.0));
 
-        //12
+        // 12
         ResidualNetwork<Double> test_network_12=new ResidualNetwork<>();
         test_network_12.setSource(new Node("a"));
         test_network_12.setDestination(new Node("d"));
         test_network_12.addEdge(new Node("a"),new Node("b"),new EdgeProperties<>(2.0,0.0));
         test_network_12.addEdge(new Node("c"),new Node("d"),new EdgeProperties<>(2.0,0.0));
 
-        //13
+        // 13
         ResidualNetwork<Double> test_network_13 = new ResidualNetwork<>();
         test_network_13.setSource(new Node("a"));
         test_network_13.setDestination(new Node("h"));
@@ -221,7 +173,7 @@ public class AlgorithmExecutorTest {
         test_network_13.addEdge(new Node("f"), new Node("g"), new EdgeProperties<>(4.0,0.0));
         test_network_13.addEdge(new Node("f"), new Node("h"), new EdgeProperties<>(4.0,0.0));
 
-        //14
+        // 14
         ResidualNetwork<Double> test_network_14 = new ResidualNetwork<>();
         test_network_14.setSource(new Node("a"));
         test_network_14.setDestination(new Node("h"));
@@ -231,7 +183,7 @@ public class AlgorithmExecutorTest {
         test_network_14.addEdge(new Node("f"), new Node("g"), new EdgeProperties<>(4.0,0.0));
         test_network_14.addEdge(new Node("f"), new Node("h"), new EdgeProperties<>(4.0,0.0));
 
-        //15
+        // 15
         ResidualNetwork<Double> test_network_15 = new ResidualNetwork<>();
         test_network_15.setSource(new Node("a"));
         test_network_15.setDestination(new Node("d"));
@@ -241,7 +193,7 @@ public class AlgorithmExecutorTest {
         test_network_15.addEdge(new Node("b"), new Node("d"), new EdgeProperties<>(0.1,0.0));
         test_network_15.addEdge(new Node("c"), new Node("d"), new EdgeProperties<>(0.2,0.0));
 
-        //16
+        // 16
         ResidualNetwork<Double> test_network_16 = new ResidualNetwork<>();
         test_network_16.setSource(new Node("a"));
         test_network_16.setDestination(new Node("f"));
@@ -253,10 +205,10 @@ public class AlgorithmExecutorTest {
         test_network_16.addEdge(new Node("d"),new Node("f"),new EdgeProperties<>(-6.0,0.0));
         test_network_16.addEdge(new Node("e"),new Node("c"),new EdgeProperties<>(-6.0,0.0));
 
-        //17
+        // 17
         ResidualNetwork<Double> test_network_17 = new ResidualNetwork<>();
 
-        //18
+        // 18
         ResidualNetwork<Double> test_network_18 = new ResidualNetwork<>();
         test_network_18.setSource(new Node("a"));
         test_network_18.setDestination(new Node("f"));
